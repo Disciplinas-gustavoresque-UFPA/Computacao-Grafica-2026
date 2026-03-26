229b8455ee4f424beda353076511ad4817d0bce1
RafaelDmiranda <rafael.miranda@icen.ufpa.br>
2026-03-26 09:27:57 -0300

    feat: implementar logica de zoom baseada em escala

--- js/tools/LupaTool.js
+++ js/tools/LupaTool.js
@@ -1,17 +1,22 @@
 import { ToolBase } from './ToolBase.js';
 
+// ferramenta responsavel por controlar zoom no canvas SVG via viewBox
 export class LupaTool extends ToolBase {
   constructor(svg) {
     super();
     this.svg = svg;
+
+    // Estado interno do SVG : "área visivel"  
     this.viewBox = {
       x: 0,
       y: 0,
       width: svg.clientWidth,
       height: svg.clientHeight
-    }; // <-- criar viewBox
-  } // <-- construtor
+    }; // <-- viewBox
+  } // <-- constructor
   
+
+  // Aplica o estado atual do viewBox no SVG : atualiza o "zoom"
   applyViewBox() {
     const {
       x,
@@ -20,7 +25,23 @@ export class LupaTool extends ToolBase {
       height
     } = this.viewBox;
     this.svg.setAttribute('viewBox','${x} ${y} ${width} ${height}');
-  } // <-- aplicar viewBox
-} // <-- classe LupaTool
+  } // <-- applyViewBox
+
+  // Realiza zoom mantendo o ponto (cx, cy) fixo como foco
+  zoom(scale, cx, cy) {
+    const newWidth = this.viewBox.width * scale;
+    const newHeight = this.viewBox.height * scale;
+  
+    // Reposiciona o viewBox para manter o cursor fixo
+    this.viewBox.x = cx - (cx - this.viewBox.x) * (newWidth / this.viewBox.width);
+    this.viewBox.y = cy - (cy - this.viewBox.y) * (newHeight / this.viewBox.height);
+    
+    // Atualiza as dimensões do viewBox 
+    this.viewBox.width = newWidth;
+    this.viewBox.height = newHeight;
+
+    this.applyViewBox();
+  }
+} // <-- class LupaTool
 
 
