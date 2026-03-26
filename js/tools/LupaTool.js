9c0bbd81c5ea94168c91cd404905527f83b440cc
RafaelDmiranda <rafael.miranda@icen.ufpa.br>
2026-03-26 09:03:27 -0300

    feat: adicionar controle inicial do viewBox na LupaTool

--- js/tools/LupaTool.js
+++ js/tools/LupaTool.js
@@ -4,7 +4,14 @@ export class LupaTool extends ToolBase {
   constructor(svg) {
     super();
     this.svg = svg;
+    this.viewBox = {
+      x: 0,
+      y: 0,
+      width: svg.clientWidth,
+      height: svg.clientHeight
+    };
   }
+
 }
 
 
