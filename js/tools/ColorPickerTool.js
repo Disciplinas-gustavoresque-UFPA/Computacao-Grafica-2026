96c7818346abcdc1682e9f1399ce7f6816378928
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2026-03-27 11:55:04 -0300

    Define cor de preenchimento ao clicar
    `ColorPickerTool.js`
    No método onMouseDown, adiciona lógica de detectar a cor da forma geométrica svg sob o cursor.
    Define a cor detectada como preenchimento ao clicar.
    
    `colorHelper.js`
    Utilitário para converter string rgb para hexadecimal.

--- js/tools/ColorPickerTool.js
+++ js/tools/ColorPickerTool.js
@@ -1,3 +1,6 @@
+import { definirCorPreenchimento } from "../core/StateManager.js";
+import { rgbToHex } from "../utils/colorHelpers.js";
+import { obterCoordenadaSVG } from "../utils/svgHelpers.js";
 import { ToolBase } from "./ToolBase.js";
 
 export class ColorPickerTool extends ToolBase {
@@ -5,5 +8,22 @@ export class ColorPickerTool extends ToolBase {
   constructor(svgCanvas) {
     super();
     this.svgCanvas = svgCanvas;
+  }
+
+  /** @param {MouseEvent} evento*/
+  onMouseDown(evento) {
+    const elemento = document.elementFromPoint(evento.x, evento.y);
+    if (!(elemento instanceof SVGGeometryElement)) return;
+    const point = obterCoordenadaSVG(evento, this.svgCanvas);
+    const isInStroke = elemento.isPointInStroke(point);
+    const style = window.getComputedStyle(elemento);
+    const color =
+      isInStroke && style.stroke !== "none" ? style.stroke : style.fill;
+    definirCorPreenchimento(color);
+
+    const corInput =
+      /** @type {HTMLInputElement} */
+      (document.getElementById("cor-preenchimento"));
+    corInput.value = rgbToHex(color);
   }
 }
---
+++ js/utils/colorHelpers.js
@@ -0,0 +1,19 @@
+const COLOR_COMPONENTS_REGEX = /\d+/g;
+
+/**
+ * Coverte uma string de cor RGB dos formatos
+ * - rgb(0, 0, 0)
+ * - (0, 0, 0)
+ *
+ * Para o formato hexadecimal #000000
+ *
+ * @param {string} rgb
+ */
+export function rgbToHex(rgb) {
+  const colorComponents = Array.from(
+    rgb.matchAll(COLOR_COMPONENTS_REGEX),
+  ).flatMap((match) => parseInt(match[0]));
+  const colorComponentsHex = colorComponents.map((color) => color.toString(16));
+  const hexString = `#${colorComponentsHex.join("")}`;
+  return hexString;
+}
