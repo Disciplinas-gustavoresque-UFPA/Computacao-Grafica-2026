bbf3830673f06ddae8ea64b993bc46b878b83e13
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2026-03-30 10:09:42 -0300

    feat: adicionando construtor da classe EllipseTool
    Criando a classe EllipseTool, extendendo de ToolBase e criando o construtor da classe, se baseando na superclasse e adicionando os atributos da classe

--- js/tools/EllipseTool.js
+++ js/tools/EllipseTool.js
@@ -1,1 +1,12 @@
 import { ToolBase } from "./ToolBase";
+
+export class EllipseTool extends ToolBase {
+    constructor(svgCanvas) {
+        super()
+        this.svgCanvas = svgCanvas
+        this.isDrawing = false
+        this.startX = 0
+        this.startY = 0
+        this.ellipseElement = null
+    }
+}
