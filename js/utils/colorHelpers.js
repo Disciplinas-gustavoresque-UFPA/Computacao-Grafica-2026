8a5a3608d0ec27d88c05eebafe2ced769ecb593d
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2026-03-31 09:30:33 -0300

    Adiciona padding na conversão de RGB para hexadecimal

--- js/utils/colorHelpers.js
+++ js/utils/colorHelpers.js
@@ -13,7 +13,9 @@ export function rgbToHex(rgb) {
   const colorComponents = Array.from(
     rgb.matchAll(COLOR_COMPONENTS_REGEX),
   ).flatMap((match) => parseInt(match[0]));
-  const colorComponentsHex = colorComponents.map((color) => color.toString(16));
+  const colorComponentsHex = colorComponents.map((color) =>
+    color.toString(16).padStart(2, "0"),
+  );
   const hexString = `#${colorComponentsHex.join("")}`;
   return hexString;
 }
