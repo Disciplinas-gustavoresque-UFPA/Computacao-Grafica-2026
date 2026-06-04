2071ca7d73afdf0c303fa4b29e6d765f92076664
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-06-04 17:44:42 -0300

    feat: adiciona as funções undo e redo
    funções que serão chamadas quando o usuário quiser restaurar estados do sistema

--- js/core/HistoryManager.js
+++ js/core/HistoryManager.js
@@ -22,4 +22,39 @@ export class HistoryManager {
     // Guarda na pilha principal
     this.#undoStack.push(snapshot);
   }
+
+  /**
+   * Volta um passo na linha do tempo (Ctrl+Z).
+  */
+  desfazer() {
+    // Precisamos de no mínimo 2 itens: o estado que vamos remover e o anterior para onde vamos voltar
+    if (this.#undoStack.length > 1) {
+      // 1. Tira o estado atual do topo da pilha de Undo e guarda no Redo
+      const estadoAtual = this.#undoStack.pop();
+      this.#redoStack.push(estadoAtual);
+
+      // 2. Olha para o estado que ficou no topo (sem remover)
+      const estadoAnterior = this.#undoStack[this.#undoStack.length - 1];
+      
+      // 3. Substitui o Canvas por esse estado anterior
+      this.#aplicarEstado(estadoAnterior.getConteudo());
+    }
+  }
+
+  /**
+   * Avança um passo na linha do tempo (Ctrl+Y ou Ctrl+Shift+Z).
+   */
+  refazer() {
+    // Só podemos refazer se houver algo aguardando na pilha de Redo
+    if (this.#redoStack.length > 0) {
+      // 1. Tira o estado do topo do Redo
+      const proximoEstado = this.#redoStack.pop();
+      
+      // 2. Devolve para a pilha de histórico principal (Undo)
+      this.#undoStack.push(proximoEstado);
+      
+      // 3. Substitui o Canvas por esse próximo estado
+      this.#aplicarEstado(proximoEstado.getConteudo());
+    }
+  }
 }
