6f22cca6b4a84d32f3471b78c7f1038e0f4671ef
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-06-04 17:31:32 -0300

    feat: adiciona função salvarEstado
    função que registra um novo estado, toda vez que o usuário executa uma nova ação

--- js/core/HistoryManager.js
+++ js/core/HistoryManager.js
@@ -7,5 +7,19 @@ export class HistoryManager {
 
   constructor(svgCanvas) {
     this.#svgCanvas = svgCanvas;
+
+    // Assim que o editor inicia, salva a tela inicial, garantindo que sempre haja um ponto seguro para retornar no final do Ctrl+Z
+    this.salvarEstado();
+  }
+
+  salvarEstado() {
+    // Toda vez que o usuário faz uma NOVA ação, o futuro alternativo (Redo) é descartado
+    this.#redoStack = [];
+    
+    // Tira a "foto" do HTML interno atual do canvas
+    const snapshot = new MementoSVG(this.#svgCanvas.innerHTML);
+    
+    // Guarda na pilha principal
+    this.#undoStack.push(snapshot);
   }
 }
