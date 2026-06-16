b319fbb700c3f6c3fea9072f3f0852bc9d9f0a7a
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-06-15 22:37:31 -0300

    feat: adiciona funções de ativação e desativação da ferramenta

--- js/tools/PoligonoPolilinhaTool.js
+++ js/tools/PoligonoPolilinhaTool.js
@@ -4,4 +4,19 @@ export class PoligonoPolilinhaTool extends ToolBase {
     constructor(svgCanvas) {
         super();
     }
+
+    /**
+    * Ativa a ferramenta e começa a escutar o teclado para o "Enter".
+    */
+    onAtivar() {
+        window.addEventListener('keydown', this.onKeyDownBound);
+    }
+
+    /**
+    * Desativa a ferramenta, limpa listeners e cancela desenhos inacabados.
+    */
+    onDesativar() {
+        window.removeEventListener('keydown', this.onKeyDownBound);
+        this.resetarDesenho();
+    }
 }
