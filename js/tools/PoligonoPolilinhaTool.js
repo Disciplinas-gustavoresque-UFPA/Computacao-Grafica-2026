73d0ba9ae84fefed5d92ec492c70edb62b69ea91
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-06-15 22:42:24 -0300

    feat: cria função para adicionar novo vértice
    Ao clicar no mouse (onMouseDown), a função cria novo vértice

--- js/tools/PoligonoPolilinhaTool.js
+++ js/tools/PoligonoPolilinhaTool.js
@@ -19,4 +19,27 @@ export class PoligonoPolilinhaTool extends ToolBase {
         window.removeEventListener('keydown', this.onKeyDownBound);
         this.resetarDesenho();
     }
+
+    /**
+    * Cada clique adiciona um novo vértice ao polígono.
+    */
+    onMouseDown(evento) {
+        // Captura a coordenada do clique adaptada ao SVG
+        const pt = obterCoordenadaSVG(evento, this.svgCanvas);
+        this.vertices.push(pt);
+
+        if (!this.polygonElement) {
+        // Cria o elemento polígono no primeiro clique
+        this.polygonElement = criarElementoSVG('polygon', {
+            points: this.formatarPoints(),
+            stroke: estado.corBorda,
+            'stroke-width': 2,
+            fill: 'transparent' // Permite ver através enquanto desenha
+        });
+        this.svgCanvas.appendChild(this.polygonElement);
+        } else {
+        // Atualiza os pontos com o novo vértice inserido
+        this.polygonElement.setAttribute('points', this.formatarPoints());
+        }
+    }
 }
