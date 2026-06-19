8aa5570581be6dbb5bb04cf6f2efe35435169dec
unknown <washington.smith2002@gmail.com>
2026-06-19 18:52:12 -0300

    fix: corrige erro na atualizacao dos botoes de undo e redo

--- js/core/HistoryManager.js
+++ js/core/HistoryManager.js
@@ -16,12 +16,26 @@ export class HistoryManager {
   salvarEstado() {
     // Toda vez que o usuário faz uma NOVA ação, o futuro alternativo (Redo) é descartado
     this.#redoStack = [];
-    
+
     // Tira a "foto" do HTML interno atual do canvas
     const snapshot = new MementoSVG(this.#svgCanvas.innerHTML);
-    
+
     // Guarda na pilha principal
     this.#undoStack.push(snapshot);
+  }
+
+  /**
+ * Indica se há ações disponíveis para desfazer.
+ */
+  podeDesfazer() {
+    return this.#undoStack.length > 1;
+  }
+
+  /**
+   * Indica se há ações disponíveis para refazer.
+   */
+  podeRefazer() {
+    return this.#redoStack.length > 0;
   }
 
   /**
@@ -36,7 +50,7 @@ export class HistoryManager {
 
       // 2. Olha para o estado que ficou no topo (sem remover)
       const estadoAnterior = this.#undoStack[this.#undoStack.length - 1];
-      
+
       // 3. Substitui o Canvas por esse estado anterior
       this.#aplicarEstado(estadoAnterior.getConteudo());
     }
@@ -50,10 +64,10 @@ export class HistoryManager {
     if (this.#redoStack.length > 0) {
       // 1. Tira o estado do topo do Redo
       const proximoEstado = this.#redoStack.pop();
-      
+
       // 2. Devolve para a pilha de histórico principal (Undo)
       this.#undoStack.push(proximoEstado);
-      
+
       // 3. Substitui o Canvas por esse próximo estado
       this.#aplicarEstado(proximoEstado.getConteudo());
     }
