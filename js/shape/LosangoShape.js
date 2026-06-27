fb173937b8a245e8acdb8adb405246479d104588
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-06-27 12:43:59 -0300

    feat: adiciona função para mudar a forma do Losango

--- js/shape/LosangoShape.js
+++ js/shape/LosangoShape.js
@@ -21,4 +21,31 @@ export class LosangoShape extends ShapeBase {
         // Renderiza cada vértice identificado
         vertices.forEach(ponto => super.renderizarHandle(ponto));
     }
+
+    atualizarForma(coords, targetElement, activateNode) {
+        const x = parseFloat(targetElement.getAttribute('x'));
+        const y = parseFloat(targetElement.getAttribute('y'));
+        const w = parseFloat(targetElement.getAttribute('width'));
+        const h = parseFloat(targetElement.getAttribute('height'));
+
+        switch(activateNode) {
+            case 'top':
+                targetElement.setAttribute('y', coords.y);
+                targetElement.setAttribute('height', Math.max(0, (y + h) - coords.y));
+                break;
+            case 'left':
+                targetElement.setAttribute('x', coords.x);
+                targetElement.setAttribute('width', Math.max(0, (x + w) - coords.x));
+                break;
+            case 'right':
+                targetElement.setAttribute('width', coords.x - x);
+                break;
+            case 'bottom':
+                targetElement.setAttribute('height', coords.y - y);
+                break;
+            
+            // Sincroniza os nodes (vértices)
+            this.sincronizarTodosOsHandles(targetElement);
+        }
+    }
 }
