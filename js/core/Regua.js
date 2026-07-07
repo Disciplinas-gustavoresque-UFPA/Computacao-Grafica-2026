6c09545de60c4a0b174f0a516dec70a5db4e33b8
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-07-07 17:55:18 -0300

    feat(Regua.js): adiciona função para criar elementos

--- js/core/Regua.js
+++ js/core/Regua.js
@@ -3,5 +3,22 @@ export class Regua {
     this.container = container;
     this.svgCanvas = svgCanvas;
     this.ativa = false;
+
+    this._criarElementos();
+  }
+
+  _criarElementos() {
+    this.horizontal = document.createElement('div');
+    this.horizontal.className = 'regua regua-horizontal oculto';
+
+    this.vertical = document.createElement('div');
+    this.vertical.className = 'regua regua-vertical oculto';
+
+    this.canto = document.createElement('div');
+    this.canto.className = 'regua-canto oculto';
+
+    this.container.appendChild(this.horizontal);
+    this.container.appendChild(this.vertical);
+    this.container.appendChild(this.canto);
   }
 }
