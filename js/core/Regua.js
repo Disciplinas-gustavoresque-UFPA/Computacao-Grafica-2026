6de4f6e32d0fdc6fed2b82627c9920c60023b17e
rogeriobarbosa07 <rogeriodosanjosbarbosa2004@gmail.com>
2026-07-07 17:55:54 -0300

    feat(Regua.js): adiciona função para registrar observadores

--- js/core/Regua.js
+++ js/core/Regua.js
@@ -5,6 +5,7 @@ export class Regua {
     this.ativa = false;
 
     this._criarElementos();
+    this._registrarObservadores();
   }
 
   _criarElementos() {
@@ -20,5 +21,28 @@ export class Regua {
     this.container.appendChild(this.horizontal);
     this.container.appendChild(this.vertical);
     this.container.appendChild(this.canto);
+  }
+
+  _registrarObservadores() {
+    // Reage a mudanças no viewBox (zoom/pan feitos pela CameraSVG)
+    this._mutationObserver = new MutationObserver((mutations) => {
+      if (!this.ativa) return;
+      for (const m of mutations) {
+        if (m.attributeName === 'viewBox') {
+          this.atualizar();
+          break;
+        }
+      }
+    });
+    this._mutationObserver.observe(this.svgCanvas, {
+      attributes: true,
+      attributeFilter: ['viewBox'],
+    });
+
+    // Reage a redimensionamento do canvas (janela, toggle da sidebar, etc.)
+    this._resizeObserver = new ResizeObserver(() => {
+      if (this.ativa) this.atualizar();
+    });
+    this._resizeObserver.observe(this.svgCanvas);
   }
 }
